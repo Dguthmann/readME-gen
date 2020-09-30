52f735f720945ade227ed2fac5b64469240db947
David Guthmann <drguthmann@cox.net>
2020-09-30 16:47:26 -0500

    fixing so it won't take package-lock

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+// npm init
+//install my dependencies -  inquirer
+
+
 // array of questions for user
 const questions = [
 
 ];
+
+//Writing a readme in a markdown file (use my template)
+
+// function that will generate my readme template
+
+// use inquirer to prompt readme questions
+
+// use answers that come back from inquirer - pass those into my generate readme function
 
 // function to write README file
 function writeToFile(fileName, data) {
