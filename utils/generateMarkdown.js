08e3d3448d2391632ee498967d0a321ad7b88b74
David Guthmann <drguthmann@cox.net>
2020-10-01 13:34:44 -0500

    added structure to generateMarkup

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  # day-planner-moment
+
+  ### Author: David Guthmann
+  
+  github: https://github.com/Dguthmann/  
+  livesite: https://dguthmann.github.io/day-planner-moment/
+  
+  ## List of Contents
+  
+  ${data.html}  
+  ${data.css}  
+  ${data.js}  
+  README.md  
+  ${data.screenshot}  
+    
+  ## Basic Overview of Project
+  
+  ${data.overview}
+  
+  
+  ## Screenshot of Website
+  
+  ![Site Screenshot](${data.screenshot})
+  
+  ## resources Used
+  
+  ${data.resources}
+  
+  ## Further Development Plans
+  
+  ${data.future}
 
 `;
 }
